7a5cdb486b802249f5082b7b33096a00c73d02c8
Yuan. Y <yuanyuanusa@gmail.com>
2018-07-12 22:13:13 -0400

    Add files via upload

---
+++ Monty-Hall-Problem.R
@@ -0,0 +1,14 @@
+doors <- c("A", "B" , "C")
+
+xdata = c()
+for (i in 1:10000)
+{prize <- sample(doors)[1]
+pick <- sample(doors)[1]
+open <- sample(doors[which(doors !=pick & doors != prize)])[1]
+switchyes <- doors[which(doors !=pick & doors != open)]
+if (pick == prize){xdata=c(xdata,"noswitchwin")}
+if(switchyes == prize){xdata=c(xdata,"switchwin")}
+}
+
+length(which(xdata == "switchwin"))
+length(which(xdata == "noswitchwin"))
